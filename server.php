f5c0fcef4536ddd07a83bf26b5a006e4427f114b
Yinhao Wang <njwangyh@gmail.com>
2017-08-13 14:23:47 +0800

    first commit

--- server.php
+++ server.php
@@ -3,7 +3,7 @@ $img =$_POST['img'];
 $id =$_POST['id'];
 $time=$_POST['time'];
 $integer=floor($time);
-$floor=$time-$integer;
+$floor=round(($time-$integer)*24);
 list($type, $data) = explode(',', $img);  
 $photo ="./tmp/video1/".$id.'_'.$integer.'_'.$floor.'.jpg';
 file_put_contents($photo, base64_decode($data), true);
